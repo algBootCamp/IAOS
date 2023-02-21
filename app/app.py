57123412586ce85d83fa8d39a1116bc958acb940
carl <1393654034@qq.com>
2023-02-21 23:48:28 +0800

    fix: logs config

--- app/app.py
+++ app/app.py
@@ -22,7 +22,7 @@ print('IAOS Server %s on %s' % (sys.version, sys.platform))
 # get golbal_cfg
 global_cfg = GlobalCfg()
 
-# ----  log init ------ #
+# ---- global log init ------ #
 log_files = global_cfg.get_log_files()
 # 确保log file path存在 python logging不提供创建日志文件路径
 for file_list in log_files.values():
@@ -34,19 +34,22 @@ LOG_CFG = "conf/logging.cfg"
 self_path = __file__
 log_cfg_path = self_path.split("app.py", 1)[0] + LOG_CFG
 logging.config.fileConfig(LOG_CFG)
-# app,log_quantization,log_schedtask,log_blueprint,log_analysis
+# app,log_quantization,log_schedtask,log_blueprint,log_analysis,log_err
 log = logging.getLogger("app")
+log_err = logging.getLogger("log_err")
 
 
-# ----  log init ------ #
+# ---- global log init ------ #
 
 
 class IAOSFlask(Flask):
 
     def make_response(self, rv):
-        """自定义响应内容 :
+        """
+        自定义响应内容 :
         视图函数可以直接返回: DataFrame,str,list、dict、tuple、None
-        如果是DataFrame可以先转为dict"""
+        如果是DataFrame可以先转为dict
+        """
         if rv is None or isinstance(rv, (str, list, dict, tuple)):
             rv = JsonResponse.success(rv)
         # 如果是 DataFrame，转为 JsonResponse
@@ -56,31 +59,34 @@ class IAOSFlask(Flask):
 
 
 def init():
-    # 常用基础数据缓存 TODO
+    # TODO
+    """常用基础数据缓存"""
     BasicDataCache().refresh()
-    log.debug("常用基础数据缓存完成。")
 
 
 # start the app
 # noinspection SpellCheckingInspection
 def start():
-    # init
-    init()
-
-    # start web server
-    server_info = global_cfg.get_server_info()
-    # flask app build
-    app = IAOSFlask(__name__)
-    # 蓝图  简单理解蓝图：就是将系统的代码模块化（组件化）
-    app.register_blueprint(blue)
-    app.config['DEBUG'] = False
-    # app.config['SECRET_KEY'] = 'ABCDEFG'
-    # 防止中文转换成ASCII编码
-    app.config['JSON_AS_ASCII'] = False
-    # 跨域的解决方案
-    # CORS(app, supports_credentials=True)
-    app.run(host=server_info['host'], port=int(server_info['port']))
-    log.debug("IAOS Server Start!")
+    try:
+        # init
+        init()
+        # start web server
+        server_info = global_cfg.get_server_info()
+        # flask app build
+        app = IAOSFlask(__name__)
+        # 蓝图  简单理解蓝图：就是将系统的代码模块化（组件化）
+        app.register_blueprint(blue)
+        app.config['DEBUG'] = False
+        # app.config['SECRET_KEY'] = 'ABCDEFG'
+        # 防止中文转换成ASCII编码
+        app.config['JSON_AS_ASCII'] = False
+        # 跨域的解决方案
+        # CORS(app, supports_credentials=True)
+        app.run(host=server_info['host'], port=int(server_info['port']))
+        log.info("IAOS Server Start!")
+    except Exception as e:
+        log_err.error("IAOS Server Failed! ", e)
+        raise Exception("IAOS Server Failed! %s" % e)
 
 
 if __name__ == '__main__':
