134814cc3b079818daf44ddf8542f25b9cb7b8a9
carl <1393654034@qq.com>
2023-02-25 01:47:34 +0800

    del key infos

--- bin/initenv.sh
+++
@@ -1,2 +0,0 @@
-# 初始化app运行的虚拟环境
-../iaosenv/bin/activate
--- bin/start.sh
+++
@@ -1,7 +0,0 @@
-# app运行脚本
-../iaosenv/bin/activate
-export FLASK_APP=../app/app
-#flask run --reload --debugger -p 8888
-# reload：热加载  debugger：debug模式
-#flask run --reload -p 8888
-flask run -h 127.0.0.1 -p 8888
